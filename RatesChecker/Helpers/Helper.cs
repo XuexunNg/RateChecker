421ab375c5b3397c29d857a9a4b00d2b015d6ab8
EricNg <eric@utrix.com.sg>
2019-04-24 21:49:14 +0800

    Added validation to ensure input dates are before today date

--- RatesChecker/Helpers/Helper.cs
+++ RatesChecker/Helpers/Helper.cs
@@ -11,14 +11,25 @@ namespace RatesChecker.Helpers
         {
             try
             {
-                return DateTime.ParseExact(date, "MMM-yyyy", CultureInfo.InvariantCulture).ToString("yyyy-MM");
+                var convertedDate = DateTime.ParseExact(date, "MMM-yyyy", CultureInfo.InvariantCulture);
+                
+                if (convertedDate >= DateTime.Now)
+                {
+                    throw new ArgumentException("Date cannot be later than today date");
+                }
+
+                return convertedDate.ToString("yyyy-MM");
             }
-            catch
+            catch(ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return "";
+            }
+            catch(SystemException ex)
             {
                 Console.WriteLine("Invalid Date Format. The date format should be MMM-yyyy (i.e Jan-2017)");
                 return "";
             }
-
         }
     }
 }
